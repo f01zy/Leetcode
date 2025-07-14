a4c888e8c6fe96075b7d58a56ee585168b8af848
f01zy <aminovali015@icloud.com>
2025-07-14 13:04:50 +0300

    Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/main.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void traverse(TreeNode *node, std::vector<int> &nums)
+{
+  if (node->left)
+  {
+    traverse(node->left, nums);
+  }
+
+  nums.push_back(node->val);
+
+  if (node->right)
+  {
+    traverse(node->right, nums);
+  }
+}
+
+std::vector<int> inorderTraversal(TreeNode *root)
+{
+  std::vector<int> nums;
+
+  if (!root)
+  {
+    return nums;
+  }
+
+  traverse(root, nums);
+  return nums;
+}

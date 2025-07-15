767b301f00e18cae0badf02b0dcb866fd253af04
f01zy <aminovali015@icloud.com>
2025-07-15 18:53:05 +0300

    Longest Common Prefix

---
+++ Longest Common Prefix/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+std::string longestCommonPrefix(std::vector<std::string> &strs)
+{
+  std::string result = "";
+
+  for (int i = 0; i < strs[0].size(); i++)
+  {
+    char ch = strs[0][i];
+    for (int j = 1; j < strs.size(); j++)
+    {
+      if (strs[j][i] != ch)
+      {
+        return result;
+      }
+    }
+    result.push_back(ch);
+  }
+
+  return result;
+}

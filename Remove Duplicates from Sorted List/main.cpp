a50758a76aa2c9fb4e21c282ba3b2d3ec07a74df
f01zy <aminovali015@icloud.com>
2025-07-15 11:01:57 +0300

    Remove Duplicates from Sorted List

---
+++ Remove Duplicates from Sorted List/main.cpp
@@ -0,0 +1,41 @@
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *deleteDuplicates(ListNode *head)
+{
+  if (!head || !head->next)
+  {
+    return head;
+  }
+
+  ListNode *node = head;
+  while (node && node->next)
+  {
+    if (node->val == node->next->val)
+    {
+      if (node->next->next)
+      {
+        node->next = node->next->next;
+      }
+
+      else
+      {
+        node->next = nullptr;
+        break;
+      }
+    }
+
+    else
+    {
+      node = node->next;
+    }
+  }
+
+  return head;
+}

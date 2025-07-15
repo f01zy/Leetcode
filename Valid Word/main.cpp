783ed1191c8996b721cfd35dec10b8f936a2eead
f01zy <aminovali015@icloud.com>
2025-07-15 10:43:32 +0300

    Valid Word

---
+++ Valid Word/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+
+bool isValid(std::string word)
+{
+  if (word.size() < 3)
+  {
+    return false;
+  }
+
+  std::string nums = "1234567890";
+  std::string symbols = "@#$";
+  std::string vowels = "aeiouAEIOU";
+  bool isVowel = false;
+  bool isConsonant = false;
+
+  for (char symbol : word)
+  {
+    if (symbols.find(symbol) != std::string::npos)
+    {
+      return false;
+    }
+
+    else if (vowels.find(symbol) != std::string::npos)
+    {
+      isVowel = true;
+    }
+
+    else if (nums.find(symbol) == std::string::npos)
+    {
+      isConsonant = true;
+    }
+  }
+
+  return isVowel && isConsonant;
+}

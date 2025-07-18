2d5987e1b55bad93f8adb47e7c0e20487fa8c19a
f01zy <aminovali015@icloud.com>
2025-07-18 15:38:34 +0300

    Intersection of Two Linked Lists

---
+++ Intersection of Two Linked Lists/main.cpp
@@ -0,0 +1,25 @@
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+  ListNode *tailA = headA;
+  while (tailA->next)
+  {
+    tailA = tailA->next;
+  }
+  ListNode *node = headB;
+  while (node)
+  {
+    if (node >= headA && node <= tailA)
+    {
+      return node;
+    }
+    node = node->next;
+  }
+  return nullptr;
+}

0bef8cd1aee594e5930e1865e6edc2274b2c2f24
f01zy <aminovali015@icloud.com>
2025-07-18 15:24:04 +0300

    Linked List Cycle

---
+++ Linked List Cycle/main.cpp
@@ -0,0 +1,20 @@
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+bool hasCycle(ListNode *node)
+{
+  while (node)
+  {
+    if (node->next && node->next <= node)
+    {
+      return true;
+    }
+    node = node->next;
+  }
+
+  return false;
+}

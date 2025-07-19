fbe4cf7f1a7e2079dc000121c6576d89cd789197
f01zy <aminovali015@icloud.com>
2025-07-19 14:47:43 +0300

    Palindrome Linked List

---
+++ Palindrome Linked List/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+bool isPalindrome(ListNode *head)
+{
+  std::vector<int> nums;
+  while (head)
+  {
+    nums.push_back(head->val);
+    head = head->next;
+  }
+  for (int i = 0; i < nums.size() / 2; i++)
+  {
+    if (nums[i] != nums[nums.size() - 1 - i])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}

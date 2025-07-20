3d3066a465073b3e4ce53f7fead3c8a4bb647117
f01zy <aminovali015@icloud.com>
2025-07-20 17:22:44 +0300

    Binary Tree Postorder Traversal

---
+++ Binary Tree Postorder Traversal/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void traverse(std::vector<int> &result, TreeNode *node)
+{
+  if (node->left)
+  {
+    traverse(result, node->left);
+  }
+
+  if (node->right)
+  {
+    traverse(result, node->right);
+  }
+
+  result.push_back(node->val);
+}
+
+std::vector<int> postorderTraversal(TreeNode *root)
+{
+  std::vector<int> result;
+  traverse(result, root);
+  return result;
+}

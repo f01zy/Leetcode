e4b758e489845ba39fbea9c63d467f86bbd1ec47
f01zy <aminovali015@icloud.com>
2025-07-21 11:49:31 +0300

    Delete Characters to Make Fancy String

---
+++ Delete Characters to Make Fancy String/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+std::string makeFancyString(std::string s)
+{
+  std::string result{s[0]};
+  for (int i = 1, j = 1; i < s.size(); i++)
+  {
+    if (s[i] == s[i - 1])
+    {
+      j++;
+    }
+    else
+    {
+      j = 1;
+    }
+    if (j <= 2)
+    {
+      result.push_back(s[i]);
+    }
+  }
+  return result;
+}

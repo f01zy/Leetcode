637f26f795880a8e0b2cc19beb3d55b3a1cbd8f2
f01zy <aminovali015@icloud.com>
2025-07-21 21:51:49 +0300

    Container With Most Water

---
+++ Container With Most Water/main.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+int maxArea(std::vector<int> &height)
+{
+  int res = 0;
+  int pl = 0;
+  int pr = height.size() - 1;
+  while (pl < pr)
+  {
+    int k = std::min(height[pl], height[pr]) * (pr - pl);
+    if (k > res)
+    {
+      res = k;
+    }
+    if (height[pl] > height[pr])
+    {
+      pr--;
+    }
+    else
+    {
+      pl++;
+    }
+  }
+  return res;
+}

829a7d0082ea8a6202cf7c95409a7c698d588103
f01zy <aminovali015@icloud.com>
2026-01-12 14:07:41 +0300

    Minimum Time Visiting All Points

---
+++ Minimum Time Visiting All Points/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+int minTimeToVisitAllPoints(std::vector<std::vector<int>> &points) {
+  int time = 0;
+  int positionX = points[0][0], positionY = points[0][1];
+  for (int i = 1; i < points.size(); i++) {
+    int pointX = points[i][0];
+    int pointY = points[i][1];
+    int areaWidth = std::abs(pointX - positionX);
+    int areaHeight = std::abs(pointY - positionY);
+    time += std::min(areaWidth, areaHeight) + std::abs(areaWidth - areaHeight);
+    positionX = pointX;
+    positionY = pointY;
+  }
+  return time;
+}
+
+int main() {
+  std::vector<std::vector<int>> points{{1, 1}, {3, 4}, {-1, 0}};
+  std::cout << minTimeToVisitAllPoints(points) << std::endl;
+}

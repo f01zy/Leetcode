261bef5be7e4cb88f625437efffb5c818b88e051
f01zy <aminovali015@icloud.com>
2026-02-14 23:10:29 +0300

    Champagne Tower

--- Champagne Tower/main.cpp
+++ Champagne Tower/main.cpp
@@ -1,20 +1,17 @@
 #include <vector>
 
 double champagneTower(int poured, int query_row, int query_glass) {
-  std::vector<std::vector<double>> glasses(100, std::vector<double>(100, 0));
-  glasses[0][0] = static_cast<double>(poured);
-  for (int i = 0; i < query_row + 1; i++) {
-    for (int j = 0; j < i + 1; j++) {
-      double currentPoured = glasses[i][j];
-      if (currentPoured > 1) {
-        double remainder = currentPoured - 1;
-        glasses[i][j] = 1;
-        if (i < 99) {
-          glasses[i + 1][j] += remainder / 2;
-          glasses[i + 1][j + 1] += remainder / 2;
-        }
+  double tower[101][101]{0};
+  tower[0][0] = static_cast<double>(poured);
+  for (int i = 0; i <= query_row; i++) {
+    for (int j = 0; j <= i; j++) {
+      if (tower[i][j] > 1) {
+        double excess = (tower[i][j] - 1) / 2;
+        tower[i][j] = 1;
+        tower[i + 1][j] += excess;
+        tower[i + 1][j + 1] += excess;
       }
     }
   }
-  return glasses[query_row][query_glass];
+  return tower[query_row][query_glass];
 }

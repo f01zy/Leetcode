25351f888433db385b487af77a7e81092f73e69a
f01zy <aminovali015@icloud.com>
2026-03-24 14:55:33 +0300

    Construct Product Matrix

--- Construct Product Matrix/main.cpp
+++ Construct Product Matrix/main.cpp
@@ -1,5 +1,3 @@
-// TODO
-
 #include <vector>
 using namespace std;
 
@@ -11,16 +9,18 @@ vector<vector<int>> constructProductMatrix(vector<vector<int>> &grid) {
       nums[i * m + j] = grid[i][j];
     }
   }
-  vector<long long> prefix(length);
-  vector<long long> suffix(length);
-  for (int i = 0; i < length; i++) {
-    prefix[i] = i == 0 ? nums[i] : (nums[i] * prefix[i - 1]) % 12345;
-    suffix[length - i - 1] = i == 0 ? nums[length - i - 1] : (nums[length - i - 1] * suffix[length - i]) % 12345;
+  vector<long long> prefix(length, 1);
+  vector<long long> suffix(length, 1);
+  for (int i = 1; i < length; i++) {
+    prefix[i] = (prefix[i - 1] * nums[i - 1]) % 12345;
+  }
+  for (int i = length - 2; i >= 0; i--) {
+    suffix[i] = (suffix[i + 1] * nums[i + 1]) % 12345;
   }
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       int k = i * m + j;
-      grid[i][j] = k == 0 ? suffix[1] : k == length - 1 ? prefix[k - 1] : prefix[k - 1] * suffix[k + 1];
+      grid[i][j] = (prefix[k] * suffix[k]) % 12345;
     }
   }
   return grid;
--- Product of Array Except Self/main.cpp
+++ Product of Array Except Self/main.cpp
@@ -3,21 +3,17 @@ using namespace std;
 
 vector<int> productExceptSelf(vector<int> &nums) {
   int n = nums.size();
-  vector<int> prefix(n);
-  vector<int> suffix(n);
-  for (int i = 0; i < n; i++) {
-    prefix[i] = i == 0 ? nums[i] : nums[i] * prefix[i - 1];
-    suffix[n - i - 1] = i == 0 ? nums[n - i - 1] : nums[n - i - 1] * suffix[n - i];
+  vector<int> prefix(n, 1);
+  vector<int> suffix(n, 1);
+  for (int i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
   }
-  vector<int> result;
+  for (int i = n - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+  }
+  vector<int> result(n);
   for (int i = 0; i < n; i++) {
-    if (i == 0) {
-      result[i] = suffix[1];
-    } else if (i == n - 1) {
-      result[i] = prefix[n - 2];
-    } else {
-      result[i] = prefix[i - 1] * suffix[i + 1];
-    }
+    result[i] = prefix[i] * suffix[i];
   }
   return result;
 }
